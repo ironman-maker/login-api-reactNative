9ba8d8b9b28fbff95039b0e408fe6c861524502f
ironman-maker <59932310+ironman-maker@users.noreply.github.com>
2021-03-23 15:02:30 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,30 @@
+this.state = {
+	validating: false
+}
 
+render() {
+	return (
+		<Container>
+			<Content>
+				<Form>
+					<Item floatingLabel>
+						<Label>Email</Label>
+						<Input onChangeText={(text) => this.setState({email:text})} />
+					</Item>
+					<Item floatingLabel last>
+						<Label>Password</Label>
+						<Input secureTextEntry onChangeText={(text) => this.setState({password:text})} />
+					</Item>
+					<Button block success style={{ marginTop: 50 }} onPress={() => {
+						if( this.state.email && this.state.password ){
+							this.validate();
+
+						}
+					}} >
+						<Text>Authenticate</Text>
+					</Button>
+				</Form>
+			</Content>
+		</Container>
+	)
+}
